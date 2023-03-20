ae46f08a78a27a9fbc3add00516f804b9e974881
Rebecca GERREY <rebeccag@live.fr>
2023-03-20 21:58:13 +0100

    JS-Fetch added on APIController for reservation.

--- assets/js/reservation.js
+++ assets/js/reservation.js
@@ -13,27 +13,27 @@ const watchSubmit = () => {
 // - les envoyer à mon back
 const backCall = (dataToSend) => {
     console.log('dataToSend', dataToSend)
-    fetch('https://127.0.0.1:8000/reservationAPI', {
+    fetch('https://127.0.0.1:8000/api', {
         method: "POST",
         body: dataToSend})
     .then((response) => response.json())
     .then((data) => {
         console.log('data', data)
+        if (data.status === 'ok') {
+            console.log('success')
+            document.querySelector('.js-reservation-ok').classList.remove('reservation-success-hide')
+            document.querySelector('.js-reservation-ok').classList.add('reservation-success')
+            document.querySelector('.js-reservation-submit').classList.remove('resa-form__flux')
+            document.querySelector('.js-reservation-submit').classList.add('resa-form-hide')
+        }
+        else if (data.status === 'nok') {
+            console.log('error')
+            document.querySelector('.js-reservation-nok').classList.remove('reservation-error-hide')
+            document.querySelector('.js-reservation-nok').classList.add('reservation-error')
+            document.querySelector('.js-reservation-submit').classList.remove('resa-form__flux')
+            document.querySelector('.js-reservation-submit').classList.add('resa-form-hide')
+        }
     })
 }
-//problème fonction asymchrone ? Erreur 'Object of class App\Controller\ReservationController could not be converted to string'
-//lors d'essai de récupération d'une donnée en particulier avant de vérifier le formulaire avec par exmeple : 
-//$numberOfGuests = $form->$this->getNumberOfGuests();
-//dd($numberOfGuests);
-//et dans la console, erreurs :
-//POST https://127.0.0.1:8000/reservationAPI 500   reservation:98
-// window.fetch @ reservation:98
-// s @ app.605e24f7.js:1
-// (anonyme) @ app.605e24f7.js:1
-//
-// VM455:1 Uncaught (in promise) SyntaxError: Unexpected token '<', "<!-- Objec"... is not valid JSON
-// Promise.then (asynchrone)
-// s @ app.605e24f7.js:1
-// (anonyme) @ app.605e24f7.js:1
 
 watchSubmit()
