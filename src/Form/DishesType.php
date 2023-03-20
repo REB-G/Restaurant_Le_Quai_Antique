42f18ed074c8c32a1869511236561e9ed6fd364d
Rebecca GERREY <rebeccag@live.fr>
2023-03-20 12:54:15 +0100

    Changed style of forms.

--- src/Form/DishesType.php
+++ src/Form/DishesType.php
@@ -3,20 +3,96 @@
 namespace App\Form;
 
 use App\Entity\Dishes;
+use App\Entity\Categories;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class DishesType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('price')
-            ->add('imageName')
-            ->add('category')
+            ->add('name', TextType::class, [
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Nom du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Description du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ]
+            ])
+            ->add('price', IntegerType::class, [
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Prix du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ]
+            ])
+            ->add('imageFile', FileType::class, [
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Image du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ],
+                'required' => false,
+                'mapped' => false,
+            ])
+            ->add('imageName', TextType::class, [
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Nom de l\'image du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ]
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Categories::class,
+                'row_attr' => [
+                    'class' => 'dishes-form__field'
+                ],
+                'label' => 'Catégorie du plat',
+                'label_attr' => [
+                    'class' => 'dishes-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'dishes-form__field--input',
+                ]
+            ])
         ;
     }
 
--- src/Form/MenusType.php
+++ src/Form/MenusType.php
@@ -3,18 +3,69 @@
 namespace App\Form;
 
 use App\Entity\Menus;
+use App\Entity\Dishes;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class MenusType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('price')
-            ->add('dish')
+            ->add('title', TextType::class, [
+                'row_attr' => [
+                    'class' => 'menus-form__field'
+                ],
+                'label' => 'Titre du menu',
+                'label_attr' => [
+                    'class' => 'menus-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'menus-form__field--input',
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'menus-form__field'
+                ],
+                'label' => 'Description du menu',
+                'label_attr' => [
+                    'class' => 'menus-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'menus-form__field--input',
+                ]
+            ])
+            ->add('price', IntegerType::class, [
+                'row_attr' => [
+                    'class' => 'menus-form__field'
+                ],
+                'label' => 'Prix du menu',
+                'label_attr' => [
+                    'class' => 'menus-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'menus-form__field--input',
+                ]
+            ])
+            ->add('dish', EntityType::class, [
+                'class' => Dishes::class,
+                'choice_label' => 'name',
+                'expanded' => true,
+                'multiple' => true,
+                'row_attr' => [
+                    'class' => 'menus-form__field'
+                ],
+                'label' => 'Plats du menu',
+                'label_attr' => [
+                    'class' => 'menus-form__field--label'
+                ],
+            ])
         ;
     }
 
