0cffe15cf6e374cbbeca6646c19cf715b0aec102
Rebecca GERREY <rebeccag@live.fr>
2023-03-20 09:51:02 +0100

    Pages of CRUD home-page styled.

--- src/Form/HomePageType.php
+++ src/Form/HomePageType.php
@@ -6,18 +6,86 @@ use App\Entity\HomePage;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class HomePageType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pageTitle')
-            ->add('welcomeText')
-            ->add('aboutTitle')
-            ->add('aboutText')
-            ->add('sectionDishesTitle')
-            ->add('sectionDishesText')
+            ->add('pageTitle', TextType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Titre de la page',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
+            ->add('welcomeText', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Texte de bienvenue',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
+            ->add('aboutTitle', TextType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Titre de la section "A propos"',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
+            ->add('aboutText', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Texte de la section "A propos"',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
+            ->add('sectionDishesTitle', TextType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Titre de la section "Nos plats phares"',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
+            ->add('sectionDishesText', TextareaType::class, [
+                'row_attr' => [
+                    'class' => 'home-page-form__field'
+                ],
+                'label' => 'Texte de présentation de la section "Nos plats phares"',
+                'label_attr' => [
+                    'class' => 'home-page-form__field--label'
+                ],
+                'attr' => [
+                    'class' => 'home-page-form__field--input',
+                ]
+            ])
         ;
     }
 
