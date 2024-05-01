c39487a0d98f871a54777a3d527d9ca3a7641e58
Rebecca GERREY <rebeccag@live.fr>
2024-05-01 15:13:28 +0200

    Style of mobile changed.

--- assets/app.js
+++ assets/app.js
@@ -6,7 +6,6 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import './styles/app.sass';
 import './styles/sass/style.sass';
 // import { Tooltip, Toast, Popover } from 'bootstrap'
 
